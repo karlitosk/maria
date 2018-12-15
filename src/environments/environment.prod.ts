acfc508868f515e890a990d8c565ca79bcdcab53
karlitosk <carteleradenoticiascom@gmail.com>
2018-12-15 18:24:46 -0300

    Instalacion y configuración de firebase y angularfire2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCuKQXXfTS4FJAC_as1C6mDwRJcML9Gj9U",
+    authDomain: "maria-c7298.firebaseapp.com",
+    databaseURL: "https://maria-c7298.firebaseio.com",
+    projectId: "maria-c7298",
+    storageBucket: "",
+    messagingSenderId: "390149967901"
+  }  
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCuKQXXfTS4FJAC_as1C6mDwRJcML9Gj9U",
+    authDomain: "maria-c7298.firebaseapp.com",
+    databaseURL: "https://maria-c7298.firebaseio.com",
+    projectId: "maria-c7298",
+    storageBucket: "",
+    messagingSenderId: "390149967901"
+  }  
 };
 
 /*
